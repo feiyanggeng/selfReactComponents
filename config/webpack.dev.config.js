5e66c6fe8f0dae26e04feae9cb959143aa58910e
feiyanggeng <1053221101@qq.com>
2020-12-24 18:14:04 +0800

    测试 lint-staged

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const webpack = require('webpack')
 
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
   template: path.resolve(__dirname, '../src/index.html')
